852589034a2a55c531a4855104386671a83463aa
zhanglinghao <zhanglinghao@sjtu.edu.cn>
2024-07-06 15:47:24 +0800

    fix grammer bugs

---
+++ core/build/.firewall_core.ko.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/zlh/Desktop/NIS3302_/core/firewall_core.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/zlh/Desktop/NIS3302_/core/firewall_core.ko /home/zlh/Desktop/NIS3302_/core/firewall_core.o /home/zlh/Desktop/NIS3302_/core/firewall_core.mod.o;  make -f ./arch/x86/Makefile.postlink /home/zlh/Desktop/NIS3302_/core/firewall_core.ko
---
+++ core/build/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/zlh/Desktop/NIS3302_/core/modules.order := {   echo /home/zlh/Desktop/NIS3302_/core/firewall_core.o; :; } > /home/zlh/Desktop/NIS3302_/core/modules.order
